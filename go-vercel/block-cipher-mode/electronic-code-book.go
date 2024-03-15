d055d3e8011158da34cee087b98231fb59cb65b6
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-15 18:48:32 +0700

    fix: ecb mode now break text into blocks first. now can handle longer messages

--- go-vercel/block-cipher-mode/electronic-code-book.go
+++ go-vercel/block-cipher-mode/electronic-code-book.go
@@ -1,15 +1,56 @@
 package blockCipherMode
 
-import "ICCBES/lib"
+import (
+	"ICCBES/lib"
+	"ICCBES/lib/utils"
+)
 
-func EncryptECB(message []byte, key []byte, encryptionAlgorithm lib.EncryptionAlgorithm) []byte {
-	// encrypt message with key using ECB mode
-	cipherText := encryptionAlgorithm(message, key)
+func EncryptECB(plainText []byte, key []byte, encryptionAlgorithm lib.EncryptionAlgorithm) []byte {
+	// split plainText into blocks
+	plainTextBlocks := utils.TextToBlocks(plainText, len(key))
+	blockLength := len(plainTextBlocks)
+	cipherTextBlocks := make([][]byte, blockLength)
+	for i := 0; i < blockLength; i++ {
+		currentBlock := plainTextBlocks[i]
+	
+		// encrypt block
+		currentBlock = encryptionAlgorithm(currentBlock, key)
+
+		// save the result
+		cipherTextBlocks[i] = currentBlock
+	}
+
+	// merge blocks into one
+	cipherText := make([]byte, len(plainText))
+	for i := 0; i < blockLength; i++ {
+		for j := 0; j < len(key); j++ {
+			cipherText[i*len(key)+j] = cipherTextBlocks[i][j]
+		}
+	}
 	return cipherText
 }
 
 func DecryptECB(cipherText []byte, key []byte, decryptionAlgorithm lib.DecryptionAlgorithm) []byte {
-	// decrypt message with key using ECB mode
-	message := decryptionAlgorithm(cipherText, key)
-	return message
+	// split cipherText into blocks
+	cipherTextBlocks := utils.TextToBlocks(cipherText, len(key))
+	blockLength := len(cipherTextBlocks)
+	plainTextBlocks := make([][]byte, blockLength)
+	for i := 0; i < blockLength; i++ {
+		currentBlock := cipherTextBlocks[i]
+		
+		// decyrpt block
+		currentBlock = decryptionAlgorithm(currentBlock, key)
+
+		// save the result
+		plainTextBlocks[i] = currentBlock
+	}
+
+	// merge blocks into one
+	plainText := make([]byte, len(cipherText))
+	for i := 0; i < blockLength; i++ {
+		for j := 0; j < len(key); j++ {
+			plainText[i*len(key)+j] = plainTextBlocks[i][j]
+		}
+	}
+	return plainText
 }
