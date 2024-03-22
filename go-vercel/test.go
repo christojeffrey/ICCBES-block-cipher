fd37f1d7281637f66cc07ead1c05a09d47eca224
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-22 12:10:05 +0700

    update test file

--- go-vercel/test.go
+++ go-vercel/test.go
@@ -27,36 +27,36 @@ func test() {
 
 	// Testing CBC
 	fmt.Println("=== CBC mode ===")
-	iv := utils.GenerateRandomByte(constant.MessageBlockByteSize)
+	iv := utils.GenerateRandomByte(constant.MessageByteSize)
 	cipherText = cipherMode.EncryptCBC(message, key, BCA.EncryptionAlgorithm, iv)
 	fmt.Println("CipherText:", string(cipherText))
 	plainText = cipherMode.DecryptCBC(cipherText, key, BCA.DecryptionAlgorithm, iv)
 	fmt.Println("PlainText:", string(plainText))
 	utils.PrintDivider()
 
-	// // Testing CFB
-	// fmt.Println("=== CFB mode ===")
-	// iv = utils.GenerateRandomByte(constant.MessageBlockByteSize) // Reuse the generated iv
-	// cipherText = cipherMode.EncryptCFB(message, key, BCA.EncryptionAlgorithm, iv)
-	// fmt.Println("CipherText:", string(cipherText))
-	// plainText = cipherMode.DecryptCFB(cipherText, key, BCA.DecryptionAlgorithm, iv)
-	// fmt.Println("PlainText:", string(plainText))
-	// utils.PrintDivider()
+	// Testing CFB
+	fmt.Println("=== CFB mode ===")
+	iv = utils.GenerateRandomByte(constant.MessageByteSize) // Reuse the generated iv
+	cipherText = cipherMode.EncryptCFB(message, key, BCA.EncryptionAlgorithm, iv)
+	fmt.Println("CipherText:", string(cipherText))
+	plainText = cipherMode.DecryptCFB(cipherText, key, BCA.DecryptionAlgorithm, iv)
+	fmt.Println("PlainText:", string(plainText))
+	utils.PrintDivider()
 
-	// // Testing OFB
-	// fmt.Println("=== OFB mode ===")
-	// iv = utils.GenerateRandomByte(constant.MessageBlockByteSize) // Reuse the generated iv (assuming valid for OFB)
-	// cipherText = cipherMode.EncryptOFB(message, key, BCA.EncryptionAlgorithm, iv)
-	// fmt.Println("CipherText:", string(cipherText))
-	// plainText = cipherMode.DecryptOFB(cipherText, key, BCA.DecryptionAlgorithm, iv)
-	// fmt.Println("PlainText:", string(plainText))
-	// utils.PrintDivider()
+	// Testing OFB
+	fmt.Println("=== OFB mode ===")
+	iv = utils.GenerateRandomByte(constant.MessageByteSize) // Reuse the generated iv (assuming valid for OFB)
+	cipherText = cipherMode.EncryptOFB(message, key, BCA.EncryptionAlgorithm, iv)
+	fmt.Println("CipherText:", string(cipherText))
+	plainText = cipherMode.DecryptOFB(cipherText, key, BCA.DecryptionAlgorithm, iv)
+	fmt.Println("PlainText:", string(plainText))
+	utils.PrintDivider()
 
-	// // Testing CTR
-	// fmt.Println("=== CTR mode ===")
-	// iv = utils.GenerateRandomByte(constant.MessageBlockByteSize) // Reuse the generated iv (assuming valid for CTR)
-	// cipherText = cipherMode.EncryptCTR(message, key, BCA.EncryptionAlgorithm, iv)
-	// fmt.Println("CipherText:", string(cipherText))
-	// plainText = cipherMode.DecryptCTR(cipherText, key, BCA.DecryptionAlgorithm, iv)
-	// fmt.Println("PlainText:", string(plainText))
+	// Testing CTR
+	fmt.Println("=== CTR mode ===")
+	iv = utils.GenerateRandomByte(constant.MessageByteSize) // Reuse the generated iv (assuming valid for CTR)
+	cipherText = cipherMode.EncryptCounter(message, key, BCA.EncryptionAlgorithm, iv)
+	fmt.Println("CipherText:", string(cipherText))
+	plainText = cipherMode.DecryptCounter(cipherText, key, BCA.DecryptionAlgorithm, iv)
+	fmt.Println("PlainText:", string(plainText))
 }
